cd2f0cbf734db2661b532a5475d715c4b7690583
Bilal1k <48079226+Bilal1k@users.noreply.github.com>
2020-12-25 16:30:13 -0500

    Update run.r

--- run.r
+++ run.r
@@ -1,4 +1,4 @@
 if (!require("shiny")) install.packages("shiny")
 
-folder_address = "C://Users//office//Documents//Shiny 3.0"
+folder_address = "C://Users//office//Documents//Reconsile app"
 runApp(folder_address, launch.browser=TRUE)
