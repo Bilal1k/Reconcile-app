b0c6aaa7a99c0395807deeb602a8c5002f498dc1
Bilal1k <48079226+Bilal1k@users.noreply.github.com>
2021-05-01 13:07:57 -0400

    Update run.r

--- run.r
+++ run.r
@@ -1,4 +1,4 @@
 if (!require("shiny")) install.packages("shiny")
 
-folder_address = "C://Users//office//Documents//Reconsile app"
+folder_address = "C://Users//"put your user name here"//Documents//Reconsile app"
 runApp(folder_address, launch.browser=TRUE)
