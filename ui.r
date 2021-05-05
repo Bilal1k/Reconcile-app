f622d0907c75b8cbcf6ba34145b7338a861fd6da
Bilal1k <48079226+Bilal1k@users.noreply.github.com>
2021-05-05 18:47:14 -0400

    fixed params names

--- ui.r
+++ ui.r
@@ -9,29 +9,29 @@ shinyUI(
         
         conditionalPanel(
             condition = "input.doctor == 'Dr.1'",
-            fileInput("RA1", "Remittance Advice File", multiple = FALSE, accept = ".csv"),
-            fileInput("direct1", "Direct Claims", multiple = FALSE, accept = ".csv")),
+            fileInput("RA", "Remittance Advice File", multiple = FALSE, accept = ".csv"),
+            fileInput("direct", "Direct Claims", multiple = FALSE, accept = ".csv")),
         
         conditionalPanel(
             condition = "input.doctor == 'Dr.2'",
-            fileInput("RA2", "Remittance Advice File", multiple = FALSE, accept = ".csv"), 
-            fileInput("direct2", "Direct Claims", multiple = FALSE, accept = ".csv")),
+            fileInput("RA", "Remittance Advice File", multiple = FALSE, accept = ".csv"), 
+            fileInput("direct", "Direct Claims", multiple = FALSE, accept = ".csv")),
         
         
         # Dr.3 is using the office's billing software to bill hospital Pt. This need to be calculated separately.
         
         conditionalPanel(
             condition = "input.doctor == 'Dr.3'",
-            fileInput("RA3", "Remittance Advice File", multiple = FALSE, accept = ".csv"),
-            fileInput("direct3", "Direct Claims", multiple = FALSE, accept = ".csv"),
+            fileInput("RA", "Remittance Advice File", multiple = FALSE, accept = ".csv"),
+            fileInput("direct", "Direct Claims", multiple = FALSE, accept = ".csv"),
             fileInput("Hospital_1", "Hospital 1 Claims", multiple = FALSE, accept = ".csv"),
             fileInput("Hospital_2", "Hospital 2 Claims", multiple = FALSE, accept = ".csv")),
         
         conditionalPanel(
             condition = "input.doctor == 'Hospitals'",
-            fileInput("RA3H", "Remittance Advice File", multiple = FALSE, accept = ".csv"),
-            fileInput("Hospital_1H", "Hospital 1 Claims", multiple = FALSE, accept = ".csv"),
-            fileInput("Hospital_2H", "Hospital 2 Claims", multiple = FALSE, accept = ".csv")),
+            fileInput("RA", "Remittance Advice File", multiple = FALSE, accept = ".csv"),
+            fileInput("Hospital_1", "Hospital 1 Claims", multiple = FALSE, accept = ".csv"),
+            fileInput("Hospital_2", "Hospital 2 Claims", multiple = FALSE, accept = ".csv")),
             
         
         downloadButton("report", "Generate report")
