05c68d18a37ba43481cc1739755c69d5de92d242
Bilal1k <48079226+Bilal1k@users.noreply.github.com>
2021-05-05 18:46:55 -0400

    made params reactive. no need to restart often

--- server.r
+++ server.r
@@ -1,4 +1,7 @@
 shinyServer(function(input, output) {
+  
+  # make rmd and file choice reactive
+  # choose Rmd file based on chosen practitioner
   Dr <- eventReactive(input$update, {
     switch(input$doctor,
            "Dr.1" = "Dr1.Rmd",
@@ -6,8 +9,20 @@ shinyServer(function(input, output) {
            "Dr.3" = "Dr3.Rmd",
            "Hospitals" = "Hospitals.Rmd")
   }, ignoreNULL = FALSE)
+  
+  # choose file names based on chosen practitioner
+  params <-  eventReactive(input$update, {
+    switch(input$doctor,
+           "Dr.1" = list(RA = input$RA$datapath, direct = input$direct$datapath),
+           "Dr.2" = list(RA = input$RA$datapath, direct = input$direct$datapath),
+           "Dr.3" = list(RA = input$RA$datapath, direct = input$direct$datapath),
+           "Hospitals" = list(RA = input$RA$datapath, direct = input$direct$datapath,
+                           Hospital_1 = input$Hospital_1$datapath,
+                           Hospital_1 = input$Hospital_2$datapath))
+  }, ignoreNULL = FALSE)
+  
+  # Build a pdf report
   output$report <- downloadHandler(
-
     filename = "report.pdf",
     content = function(file) {
       # Copy the report file to a temporary directory before processing it, in
@@ -16,20 +31,10 @@ shinyServer(function(input, output) {
       tempReport <- file.path(tempdir(), Dr())
       file.copy(Dr(), tempReport, overwrite = TRUE)
       
-      # Set up parameters to pass to Rmd document
-      params <- list(RA1 = input$RA1$datapath,
-                     RA3 = input$RA3$datapath,
-                     RA2 = input$RA2$datapath,
-                     direct1 = input$direct1$datapath,
-                     direct2 = input$direct2$datapath,
-                     direct3 = input$direct3$datapath,
-                     Hospital_1 = input$Hospital_1$datapath,
-                     Hospital_2 = input$Hospital_2$datapath,
-                     RA3H = input$RA3H$datapath,
-                     Hospital_1H = input$Hospital_1H$datapath,
-                     Hospital_2H = input$Hospital_2H$datapath)
+      # Set up reactive parameters to pass to Rmd 
+      params <- params()
       
-      # Knit the document, passing in the `params` list, and eval it in a
+      # Knit the document, passing in the `params` list, and evaluate it in a
       # child of the global environment (this isolates the code in the document
       # from the code in this app).
       Sys.setenv(RSTUDIO_PANDOC="C:/Program Files/RStudio/bin/pandoc")
